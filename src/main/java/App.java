fd548f119f0d4a314cf4a28dcc07700aff65eb16
Maurice K <74590072+bluemustard2@users.noreply.github.com>
2021-01-20 15:17:00 -0800

    Removed 'ping/pong' test function (#3)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,29 +14,6 @@ public class App {
         Logger.LogSuccess("Successfully logged in as %s!", Client.getYourself().getDiscriminatedName());
 
         meetingAnnouncement();
-
-        Client.addMessageCreateListener((message) -> {
-
-            /*
-
-                A simple ping/pong command. Whenever a user types in '>ping', we get the current time and then have the bot
-                create a message containing "Pong!". After the message sends, we run a callback and get the current time after
-                the message creation, and edit our message to contain the time it took for the bot to send the message after
-                we invoke the command.
-
-             */
-
-            if(message.getMessageContent().startsWith(">ping"))
-            {
-                long InvokeTime = System.currentTimeMillis();
-                message.getChannel().sendMessage("Pong!")
-                        .thenAccept((m) -> {
-                            long SentTime = System.currentTimeMillis();
-                            double diff = (double)(SentTime - InvokeTime) / 1000;
-                            m.edit("Pong! Took " + diff + " second(s)");
-                        });
-            }
-        });
     }
 
     public static void meetingAnnouncement() {
