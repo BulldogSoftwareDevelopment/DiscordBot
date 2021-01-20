19c113b5608b417e15e2eeedad98efa66a5c056c
Maurice Kennedy <mauricekennedy@Maurices-MacBook-Pro.local>
2021-01-20 11:08:20 -0800

    Added Cron notifications

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import it.sauronsoftware.cron4j.Scheduler;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
@@ -13,6 +14,8 @@ public class App {
                 .join();
 
         Logger.LogSuccess("Successfully logged in as %s!", Client.getYourself().getDiscriminatedName());
+
+        meetingAnnouncement();
 
         Client.addMessageCreateListener((message) -> {
 
@@ -36,7 +39,14 @@ public class App {
                         });
             }
         });
+    }
 
+    public static void meetingAnnouncement() {
+        Scheduler s = new Scheduler();
+
+        // At 2:45 each Monday, print a meeting warning message
+        s.schedule("45 14 * * Mon", () -> System.out.println("Meeting will be starting soon!"));
+        s.start();
     }
 
 
